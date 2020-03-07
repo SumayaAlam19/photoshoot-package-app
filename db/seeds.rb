24cf2b9b80cb6c9906fe4743922c4a4ba3ef4908
SumayaAlam19 <sumayaalam123@gmail.com>
2020-03-07 14:54:56 +1100

    test

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,17 +1,16 @@
 
 
-# users = [
-#     user1 = User.create(email: 'test-user1@photos.com', password: 'password'),
-#     user2 = User.create(email: 'test-user2@photos.com', password: 'password'),
-#     user3 = User.create(email: 'test-user3@photos.com', password: 'password'),
-#     user4 = User.create(email: 'test-user3@photos.com', password: 'password'),
-#     user5 = User.create(email: 'test-user3@photos.com', password: 'password')
-#     ]
+users = [
+    user1 = User.create(email: 'test-user1@photos.com', password: 'password'),
+    user2 = User.create(email: 'test-user2@photos.com', password: 'password'),
+    user3 = User.create(email: 'test-user3@photos.com', password: 'password'),
+    user4 = User.create(email: 'test-user3@photos.com', password: 'password'),
+    user5 = User.create(email: 'test-user3@photos.com', password: 'password')
+    ]
     
-# # # users.each do |user|
-#     Package.create(name:"Signature Package", price: 500.00, hours: 7, notes:"There will be 4 photographers to cover the session and some props as well ", user_id: user[0].id)
-#     Package.create(name:"Extra Large Package", price: 400.00, hours: 6, notes:"There will be 3 photographers to cover the session and some props as well ", user_id: user[1].id)
-#     Package.create(name:"Large Package", price: 300.00, hours: 4, notes:"There will be 2 photographers to cover the session and some flowers as well ", user_id: user[2].id)
-#     Package.create(name:"Medium Package", price: 200.00, hours: 3, notes:"There will be 2 photographers to cover the session and no props", user_id: user[3].id)
-#     Package.create(name:"Small Package", price: 100.00, hours: 1, notes:"There will be 1 photographers to cover the session", user_id: user[4].id)
-# # # end
+# users.each do |user|
+    # package = Package.create(name:"Signature Package", price: 500.00, hours: 7, notes:"There will be 4 photographers to cover the session and some props as well ", user_id: users[0].id)
+    # package = Package.create(name:"Extra Large Package", price: 400.00, hours: 6, notes:"There will be 3 photographers to cover the session and some props as well ", user_id: users[1].id)
+    # package = Package.create(name:"Large Package", price: 300.00, hours: 4, notes:"There will be 2 photographers to cover the session and some flowers as well ", user_id: users[2].id)
+    # package = Package.create(name:"Medium Package", price: 200.00, hours: 3, notes:"There will be 2 photographers to cover the session and no props", user_id: users[3].id)
+    # package = Package.create(name:"Small Package", price: 100.00, hours: 1, notes:"There will be 1 photographers to cover the session", user_id: users[4].id)
