e19bc5e77178e65af5a9a9ff6e6654e66a3c7526
SumayaAlam19 <sumayaalam123@gmail.com>
2020-03-11 01:43:18 +1100

    added dashboard

---
+++ app/controllers/dashboard_controller.rb
@@ -0,0 +1,8 @@
+class DashboardController < ApplicationController
+    before_action :authenticate_user!
+
+    def index
+        @packages = Package.where(user_id: current_user.id)
+        @bookings =Booking.where(user_id: current_user.id)
+    end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
   resources :packages
+  resources :dashboard, only: [:index] 
   get 'payment' => 'packages#payment'
 
   root to: 'packages#index'
